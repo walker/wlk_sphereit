5c1f9dd48bb41c51a0ec5dedeebbf538678d82b0
Walker Hamilton <walker@walker-hamiltons-macbook-pro.local>
2009-05-28 13:40:32 -0500

    removing zem

--- wlk_sphereit.php
+++ wlk_sphereit.php
@@ -6,8 +6,7 @@ $plugin['description'] = 'Put the sphereIt! widget on your site.';
 
 $plugin['type'] = 0;
 
-
-@include_once('zem_tpl.php');
+@include_once(dirname(dirname(__FILE__)).'/zem_tpl.php');
 
 if(0) {
 ?>
